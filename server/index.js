9ab6aeacbd0409d492021be8c33af930d8c25891
arkine <slyngstad55@gmail.com>
2018-08-26 11:59:20 -0600

    Added webpack setup files

--- server/index.js
+++ server/index.js
@@ -3,25 +3,18 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-    path: path.join(__dirname, '../config.js')
+    path: path.join(__dirname, '../variables.env')
 });
-
-mongoose.connect(`${process.env.db.uri}/${process.env.db.name}`);
+console.log("PROCESS HRERE", process.env.MODE)
+mongoose.connect(`${process.env.DB}`);
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', err => {
     console.log(`There was an error connecting to the DB: ${err}`);
 });
 
-const app = require('./server/src');
-
-const app = require('./src/app');
+const app = require('./src');
 const server = app.listen(process.env.PORT || 8080, () => {
 	console.log(`Server listening on port ${server.address().port}`);
-});
-
-
-app.listen(process.env.port || 8080, () => {
-    console.log(`App is now listening on port ${app.address().port}`);
 });
 
 module.exports = app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 const Express = require('express');
+const path = require('path');
 
 const app = new Express();
 
 // Where our static files are served from
-app.use("/static", express.static(path.join(__dirname, '../../build/static')));
+app.use("/static", Express.static(path.join(__dirname, '../../build/static')));
 
 // Because routing is done client-side, we want to always serve the index.html file
-app.get('*', req, res => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
+
+module.exports = app;

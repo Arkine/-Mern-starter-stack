25b6405d630df69b136d3a7a1f65b23e9f2ff3fe
arkine <slyngstad55@gmail.com>
2018-11-25 10:59:11 -0700

    added theme provider and theme settings

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
 
 import Page from 'app/components/Page';
 
+import theme from './appTheme';
 
 export default class App extends React.Component {
     render() {
         return (
-            <Page />
+            <ThemeProvider theme={theme}>
+                <Page />    
+            </ThemeProvider>
         );
     }
 }
---
+++ client/src/components/appTheme.js
@@ -0,0 +1,14 @@
+const colors = {
+    red: 'red',
+    blue: 'blue',
+    green: 'green',
+}
+
+export default {
+    colors: {
+        ...colors
+    },
+    body: {
+        rowWidth: '70rem'
+    }
+}

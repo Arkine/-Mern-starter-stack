6b95e1d00852dc0bc04df35b378c8ea99d3838c3
arkine <slyngstad55@gmail.com>
2018-12-07 20:57:42 -0700

    added adapters for the auth and db

---
+++ server/src/database/adapter.js
@@ -0,0 +1,24 @@
+export default class DatabaseAdapter {
+    constructor() {
+        super();
+
+        // The type of entity
+        this.Schema = null;
+    }
+
+    findOne() {
+        return false;
+    }
+
+    findOneById() {
+        return false;
+    }
+
+    findAll() {
+        return false;
+    }
+
+    update() {
+        return false;
+    }
+}
